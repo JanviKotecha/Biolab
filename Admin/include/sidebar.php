0841f75aa4bf97d16d93590bed45650b57c71d69
Jsmalam <jatin.jsmalam@gmail.com>
2022-01-21 04:25:34 -0500

    icon changes

--- Admin/include/sidebar.php
+++ Admin/include/sidebar.php
@@ -1,11 +1,8 @@
 <div class="nk-sidebar" >           
   <div class="nk-nav-scroll" >
     <ul class="metismenu" id="menu" >
-    
-      
-     
       <li><a href="user.php" class="<?php echo $pgName == 'courses' ? 'active' : ''; ?>"><i class="fa fa-book menu-icon"></i><span class="nav-text">Dashbord</span></a></li>
-      <li><a href="user_entry.php" class="<?php echo $pgName == 'user' ? 'active' : ''; ?>"><i class="fa fa-usergit  menu-icon"></i><span class="nav-text">User</span></a></li>
+      <li><a href="user_entry.php" class="<?php echo $pgName == 'user' ? 'active' : ''; ?>"><i class="fa fa-user  menu-icon"></i><span class="nav-text">User</span></a></li>
       <li><a href="logout.php"><i class="fa fa-power-off menu-icon"></i><span class="nav-text">Logout</span></a></li>
       <li><a href="changepwd.php"><i class="fa fa-key menu-icon"></i><span class="nav-text">ChangePassword</span></a></li>
     </ul>
